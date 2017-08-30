6f85bd15610301d09cd4baf9839017cd9e909f0c
Isaque <isaque.campello@gmail.com>
2017-08-30 20:16:33 +0000

    exercise logic working

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,20 @@
 console.log("yeah its fine");
 var submit = document.getElementById("submit");
 var answer = document.getElementById("answer");
+var correct_answer = document.getElementById("correct");
+var wrong_answer = document.getElementById("wrong");
 submit.addEventListener("click", function() {
   if (answer.value === "13") {
-    alert("YES");
+    wrong_answer.classList.add("hidden");
+    wrong_answer.classList.remove("shown");
+    correct_answer.classList.add("shown");
+    correct_answer.classList.remove("hidden");
     answer.value = "";
   } else {
-    alert("NOPE");
+    correct_answer.classList.add("hidden");
+    correct_answer.classList.remove("shown");
+    wrong_answer.classList.add("shown");
+    wrong_answer.classList.remove("hidden");
     answer.value = "";
   }
   console.log(answer.value);
