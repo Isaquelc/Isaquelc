0f830dff28c205e9a1185335a718118052ffc663
Isaque <isaque.campello@gmail.com>
2017-08-30 19:44:54 +0000

    first version of exercises

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,13 @@
 console.log("yeah its fine");
+var submit = document.getElementById("submit");
+var answer = document.getElementById("answer");
+submit.addEventListener("click", function() {
+  if (answer.value === "13") {
+    alert("YES");
+    answer.value = "";
+  } else {
+    alert("NOPE");
+    answer.value = "";
+  }
+  console.log(answer.value);
+})
