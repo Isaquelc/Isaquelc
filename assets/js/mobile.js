dc049f637ce52996574a26dd17d64bd3637027a1
Isaquelc <isaque.campello@gmail.com>
2017-08-31 02:03:58 +0000

    Delete mobile.js

--- assets/js/mobile.js
+++
@@ -1,7 +0,0 @@
-//resize window
-document.getElementById("mobile_button").addEventListener("click", function() {
-  window.resizeTo(
-    window.screen.availWidth /2,
-    window.screen.availHeight/2);
-  console.log("resize");
-});
